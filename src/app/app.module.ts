d4ff3e35cac018a7abce58fae021d3bfe4a2b52b
JeanPec <andremarie67450@gmail.com>
2024-08-19 21:20:08 +0200

    second commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [AppComponent, UserComponent, LoginComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/core/constants/apps.ts
@@ -0,0 +1,2 @@
+export const LOCAL_FR = 'fr-FR';
+export const PASSWORD = 'pwd123';
