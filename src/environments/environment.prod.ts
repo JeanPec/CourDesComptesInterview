0295cbecad9416fd3b6132348d86a39831ca2575
JeanPec <andremarie67450@gmail.com>
2024-09-05 23:12:09 +0200

    change prod api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ import { URLS } from '@app/core/constants/urls';
 export const ENVIRONNEMENT = {
   production: true,
   local: false,
-  baseUrl: 'http://localhost:3000/',
+  baseUrl: 'https://courdescomptesinterviewbe.onrender.com/',
   urls: URLS,
 };
